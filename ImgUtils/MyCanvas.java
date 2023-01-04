16ae1e5bac5865bd531ab5173084252665a5573b
alameluk17 <alamukanna14@gmail.com>
2023-01-04 13:52:19 +0530

    Use Bufferedmage to load and save image to and from editor.

--- ImgUtils/MyCanvas.java
+++ ImgUtils/MyCanvas.java
@@ -1,41 +1,44 @@
 package ImgUtils;
 
-import javax.imageio.ImageIO;
-import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+
 public class MyCanvas {
-    public static void ImageCanvasFrame(String path) {
 
-        JFrame frame = new JFrame(); //JFrame Creation       
-        frame.setTitle("Add Image"); //Add the title to frame
-        frame.setLayout(null); //Terminates default flow layout
-        frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE); //Terminate program on close button
-        int width = 1;
-        int height = 1;
+  public static void ImageCanvasFrame(String path) {
+    JFrame frame = new JFrame(); //JFrame Creation
+    frame.setTitle("Add Image"); //Add the title to frame
+    frame.setLayout(null); //Terminates default flow layout
+    frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE); //Terminate program on close button
+    int width = 1;
+    int height = 1;
+    BufferedImage bimg;
 
-        try{
-        BufferedImage bimg = ImageIO.read(new File(path));
-        width          = bimg.getWidth();
-        height         = bimg.getHeight(); 
-        }
-        catch(Exception e)
-        {
-            //never enters here
-            System.out.println("NOOOOOOO");
-        }
-        frame.setBounds(0, 0, width+100, height+50); //Sets the position of the frame
-        
-        Container c = frame.getContentPane(); //Gets the content layer
+    try {
+      bimg = ImageIO.read(new File(path));
+      width = bimg.getWidth();
+      height = bimg.getHeight();
+      frame.setBounds(0, 0, width + 100, height + 50); //Sets the position of the frame
 
-        JLabel label = new JLabel(); //JLabel Creation
-        label.setIcon(new ImageIcon(path)); //Sets the image to be displayed as an icon
-        Dimension size = label.getPreferredSize(); //Gets the size of the image
-        label.setBounds(0, 0, size.width, size.height); //Sets the location of the image
- 
-    c.add(label); //Adds objects to the container
-        frame.setVisible(true); // Exhibit the frame
+      Container c = frame.getContentPane(); //Gets the content layer
 
+      JLabel label = new JLabel(new ImageIcon(bimg)); //JLabel Creation
+      // label.setIcon(new ImageIcon(bimg)); //Sets the image to be displayed as an icon
+      Dimension size = label.getPreferredSize(); //Gets the size of the image
+      label.setBounds(0, 0, size.width, size.height); //Sets the location of the image
+
+      c.add(label); //Adds objects to the container
+      frame.setVisible(true); // Exhibit the frame
+
+    //BufferedImage bi = getMyImage();
+      File outputfile = new File("saved.png");
+      ImageIO.write(bimg, "png", outputfile);
+    } catch (Exception e) {
+      //never enters here
+      System.out.println("NOOOOOOO");
     }
+  }
 }
